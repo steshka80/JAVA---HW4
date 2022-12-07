5c454ba45ec435d357bfff16ff50700e47b2c709
steshka80 <106751602+steshka80@users.noreply.github.com>
2022-12-07 12:15:15 +0300

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,25 @@
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+import java.util.LinkedList;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        int[] listMas = { 5, 3, 2, 6, 7, 4, 9, 1 };
+        LinkedList<Integer> linkList = new LinkedList<>();
+
+        for (Integer temp : listMas) {
+            linkList.add(temp);
+        }
+
+        int temp = 0;
+        while (temp != linkList.size() - 1) {
+            linkList.add(linkList.size() - temp, linkList.getFirst());
+            linkList.removeFirst();
+            temp++;
+        }
+
+        System.out.println("linkList = " + linkList);
+
+    }
+}
